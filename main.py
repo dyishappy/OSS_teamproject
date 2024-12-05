628db6828667a14a15194613d95f49442552c6c7
youngbeen Kim <suwo5612@naver.com>
2024-12-05 11:51:22 +0900

    check main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 tokenizer = AutoTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
 model = AutoModelForCausalLM.from_pretrained("lmsys/vicuna-7b-v1.5")
+
+print(1)
